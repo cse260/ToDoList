edfefeb8e95eff5f657659fd4328fa62dad01fae
FCsimple <1580777050@qq.com>
2020-06-30 17:04:06 +0800

    修改后的文件

--- test3/src/main/java/test3/Schedule.java
+++ test3/src/main/java/test3/Schedule.java
@@ -43,4 +43,5 @@ public class Schedule {
             System.out.println("请继续你的计划表，若退出，请输入exit");
         }
     }
+    
 }
--- test3/src/main/java/test3/ThingList.java
+++ test3/src/main/java/test3/ThingList.java
@@ -64,4 +64,5 @@ public class ThingList {
     public boolean checkIsEmpty(){
         return notFinishList.isEmpty();
     }
+    
 }
